6fcab881402a2f343f7b759ac539b5016e43f396
Zaheer <zaheerhussain9620@gmail.com>
2026-03-24 09:50:58 +0530

    Initial Commit

---
+++ src/main/java/com/pu/exp7/App.java
@@ -0,0 +1,7 @@
+package com.pu.exp7;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/addnumtest.java
@@ -0,0 +1,13 @@
+package com.pu.exp7;
+
+public class addnumtest {
+
+	public static void main(String[] args) {
+		@test
+		public boolean testAddnum() {
+			assertEquals(10,addnum(7,3));
+		}
+
+	}
+
+}
